6b6b0b0aa8e8cdc06c9cea570a478e5d5aaa3b6c
Ahmad Raza <ahmadraza792003@gmail.com>
2026-04-24 17:24:48 +0500

    refactor: migrate source files to correct package structure and add Servlet API dependency to pom.xml

--- Example/src/main/java/com/example/MetricController.java
+++ Example/src/main/java/com/example/MetricController.java
@@ -1,8 +1,12 @@
+package com.example;
+
 import io.prometheus.client.Counter;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/increment")
 public class MetricController extends HttpServlet {
@@ -12,7 +16,7 @@ public class MetricController extends HttpServlet {
         .help("Total number of button clicks.")
         .register();
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         clickCounter.inc(); // This adds 1 to the Prometheus counter
     }
 }
